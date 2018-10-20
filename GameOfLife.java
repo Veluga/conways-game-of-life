b82b74e41014e9263d7c334e7d65dec429229a87
Veluga <albertboehm@hotmail.de>
2018-10-20 14:06:10 +0100

    Added printWorld and genWorld functions.

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,12 +1,38 @@
 import java.util.Scanner;
 
 public class GameOfLife {
-    public static void main(String[] args){};
+    public static void main(String[] args){
+        boolean[][] world = genWorld(10);
+        printWorld(world);
+    };
 
-    private static void printWorld(boolean[][] world) {};
+    private static void printWorld(boolean[][] world) {
+        int numRows = world.length;
+        int numCols = world[0].length;
+
+        for(int r = 0; r < numRows; r++) {
+            for(int c = 0; c < numCols; c++) {
+                if(world[r][c]) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    };
 
     private static boolean[][] genWorld(int dimension) {
-        return new boolean[dimension][dimension];
+        boolean[][] world = new boolean[dimension][dimension];
+
+        for(int r = 0; r < dimension; r++) {
+            for(int c = 0; c < dimension; c++) {
+                if(Math.random() < 0.5) {
+                    world[r][c] = true;
+                }
+            }
+        }
+        return world;
     };
 
     private static boolean[][] nextGen(boolean[][] world) {
